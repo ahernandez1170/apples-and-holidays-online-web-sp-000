875d074902f4c36501c512d3eb9a282637fed4b0
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-26 20:32:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
      decorations << item
    end
 end
+
 add_supply_to_winter_holidays(holiday_supplies, "Balloons")
 
 def add_supply_to_memorial_day(holiday_hash, supply)
